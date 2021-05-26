64d8735ce62fa6ec06f59f36fc1ed2e4a8f079ae
jvargas <vjanethh@gmail.com>
2021-05-25 23:17:04 -0500

    Subiendo proyecto terminando v1

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-
-  constructor() { }
+  producto: ProductoDescripcion;
+  id:string;
+  constructor(private route:ActivatedRoute,public productosService:ProductosService) { }
 
   ngOnInit(): void {
-  }
-
+    this.route.params.subscribe(parametros => {            
+      this.productosService.getProducto(parametros['id']).subscribe((producto: ProductoDescripcion) => {
+        this.id = parametros['id'];
+        this.producto = producto;
+      });
+    })
+  }  
 }
